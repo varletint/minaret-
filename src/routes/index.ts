2241648962612e8b1bfadaddbb514469904caa16
varletint <intvarlet@hotmail.com>
2026-02-06 01:18:47 +0100

    route index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,3 +3,4 @@ export { default as stationRoutes } from "./stationRoutes.js";
 export { default as showRoutes } from "./showRoutes.js";
 export { default as searchRoutes } from "./searchRoutes.js";
 export { default as icecastRoutes } from "./icecastRoutes.js";
+export { default as analyticsRoutes } from "./analyticsRoutes.js";
