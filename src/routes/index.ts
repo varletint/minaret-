9217903258f5b89f7f74fbab84321e2a17223a07
varletint <intvarlet@hotmail.com>
2026-02-10 02:59:13 +0100

    routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 export { default as authRoutes } from "./authRoutes.js";
+// export { default as adminRoutes } from "./adminRoutes.js";
 export { default as stationRoutes } from "./stationRoutes.js";
 export { default as showRoutes } from "./showRoutes.js";
 export { default as searchRoutes } from "./searchRoutes.js";
 export { default as icecastRoutes } from "./icecastRoutes.js";
 export { default as analyticsRoutes } from "./analyticsRoutes.js";
+export { default as recordingRoutes } from "./recordingRoutes.js";
